e1782abcaa67dd81c6b078421c32d05dc9cfcead
killatonna <vvyayfva@gmail.com>
2023-05-18 23:41:08 +0700

    Split main.c into several files
    Add course.c
    Add help_func.c
    Add help_func.h

---
+++ course.c
@@ -0,0 +1,11 @@
+#include "help_func.h"
+#include <stdio.h>
+
+int main() {
+    char program[STACK_MAX];
+    printf("Enter a program: ");
+    fgets(program, STACK_MAX, stdin);
+    int answer = is_balanced(program);
+    check(answer);
+    return 0;
+}
--- help_func.c
+++ help_func.c
@@ -1,11 +1,5 @@
+#include "help_func.h"
 #include <stdio.h>
-
-#define STACK_MAX 100
-
-typedef struct {
-    int top;
-    char data[STACK_MAX];
-} Stack;
 
 void push(Stack* stack, char c) {
     if (stack->top < STACK_MAX) {
@@ -21,6 +15,7 @@ char pop(Stack* stack) {
         stack->top--;
         return stack->data[stack->top];
     } else {
+        printf("Stack is empty\n");
         return '\0';
     }
 }
@@ -59,19 +54,10 @@ int is_balanced(char* program) {
 
 int check(int answer) {
     if (answer) {
-        printf("Balanced\n");
+        printf("balanced\n");
         return 1;
     } else {
         printf("You have mistake\n");
         return 0;
     }
-}
-
-int main() {
-    char program[STACK_MAX];
-    printf("Enter a program: ");
-    fgets(program, STACK_MAX, stdin);
-    int answer = is_balanced(program);
-    check(answer);
-    return 0;
 }
