b28c4cbecee7f22e0facf40c17ee289611508974
killatonna <vvyayfva@gmail.com>
2023-05-20 20:14:44 +0700

    Add new feature
    Reading from the program.txt file
    Add program.txt

--- src/course/course.c
+++ src/course/course.c
@@ -4,8 +4,16 @@
 
 int main() {
     char program[STACK_MAX];
-    printf("Enter a program: ");
-    fgets(program, STACK_MAX, stdin);
+    const char* file_name = "program.txt";
+
+    FILE* file = fopen(file_name, "r");
+    fseek(file, 0, SEEK_END);
+    long file_size = ftell(file);
+    rewind(file);
+
+    fread(program, sizeof(char), file_size, file);
+    fclose(file);
+
     int answer = is_balanced(program);
     check(answer);
     return 0;
